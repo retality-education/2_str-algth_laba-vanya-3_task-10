bb63f71d770287775fe9b74556f6c94616a5a357
Витя Аа <idinashyi@yandex.ru>
2024-12-10 14:14:42 +0300

    reworked

--- laba-vanya-3_task-10.cpp
+++ laba-vanya-3_task-10.cpp
@@ -1,42 +1,27 @@
 ﻿// Функция для проверки чередования гласных и согласных
 #include "Trie_Tree.h"
 #include <Windows.h>
+#include <string>
 // Функция для проверки чередования гласных и согласных
-bool is_alternating(const std::string& word) {
-    if (word.empty()) return false;
 
-    bool prev_is_vowel = (std::string("aeiou").find(tolower(word[0])) != std::string::npos);
-
-    for (size_t i = 1; i < word.size(); ++i) {
-        bool is_vowel = (std::string("aeiou").find(tolower(word[i])) != std::string::npos);
-
-        // Если текущий тип совпадает с предыдущим, не чередуется
-        if (prev_is_vowel == is_vowel) {
-            return false;
-        }
-
-        prev_is_vowel = is_vowel; // Обновляем предыдущий тип
-    }
-
-    return true; // Если все символы чередуются
-}
+const std::string gl = "eyuioa";
 
 // Функция для печати слов с чередующимися гласными и согласными
+bool is_glas(char c) {
+    return gl.find(c) != std::string::npos;
+}
+
 void print_alternating_words(ttree::ptrNODE t, std::string word) {
-    if (t->eow && is_alternating(word)) {
+    if (t->eow) {
         std::cout << word << std::endl;
     }
 
-    for (int i = 0; i < 26; i++) {
-        if (t->ptrs[i]) {
+    for (int i = 0; i < 26; i++) 
+        if (t->ptrs[i] && ( (word.length() < 2) || is_glas(char(i + 'a')) != is_glas(word[word.length() - 1]) ))
             print_alternating_words(t->ptrs[i], word + char(i + 'a'));
-        }
-    }
 }
 
-void print_alternating_vowels_consonants(ttree::ptrNODE root) {
-    print_alternating_words(root, "");
-}
+
 
 // Пример использования
 int main() {
@@ -45,7 +30,7 @@ int main() {
     ttree::TTREE trie("data.txt");
 
     std::cout << "Слова с чередующимися гласными и согласными:\n";
-    print_alternating_vowels_consonants(trie.get_root());
+    print_alternating_words(trie.get_root(), "");
 
     return 0;
 }
